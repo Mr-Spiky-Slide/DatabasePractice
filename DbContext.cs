feccc78ab5f7a17a227261bd97512a9263390aaf
JackHoward <jhoward000@live.com>
2023-04-03 13:34:35 -0500

    Use appsettings.json for private key

--- DbContext.cs
+++ DbContext.cs
@@ -1,25 +1,24 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
- public class BloggingContext : DbContext
- {
-     public DbSet<Blog> Blogs { get; set; }
-     public DbSet<Post> Posts { get; set; }
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
 
-     public void AddBlog(Blog blog)
-     {
-         this.Blogs.Add(blog);
-         this.SaveChanges();
-     }
+    public void AddBlog(Blog blog)
+    {
+        this.Blogs.Add(blog);
+        this.SaveChanges();
+    }
 
-     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-     {
-         // *******************************************************************
-         // * IT IS NOT A GOOD IDEA TO STORE USER ID AND/OR PASSWORD          *
-         // * DIRECTLY IN A CLASS (ESPECIALLY IF IT WILL BE POSTED ON GITHUB) *
-         // * WE WILL LEARN LATER HOW TO REMOVE THIS DATA FROM THE CLASS      *
-         // *******************************************************************
-         // ## last 2 digits of CRN
-         // XXX your initials
-         optionsBuilder.UseSqlServer(@"Server=bitsql.wctc.edu;Database=BlogsConsole_##_XXX;User ID=YYY;Password=ZZZ");
-     }
- }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var configuration = new ConfigurationBuilder()
+     .AddJsonFile($"appsettings.json");
+
+        var config = configuration.Build();
+        optionsBuilder.UseSqlServer(@config["BlogsConsole:ConnectionString"]);
+    }
+}
